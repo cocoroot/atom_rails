d0fd417828b6536fa9be6a6b77f7a42b5504b0dc
d318503 <satou-y93@mail.dnp.co.jp>
2016-03-04 19:08:55 +0900

    add core modules.

--- lib/atom.rb
+++ lib/atom.rb
@@ -5,8 +5,29 @@ module Atom
   class InstallGenerator < ::Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
+    def create_app_core_dir
+      dir = "#{Rails.root}/app/core"
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    end
+
+    def create_logic_base
+      template "app/core/logic_base.rb", "app/core/logic_base.rb"
+    end
+
+    def create_chain_method
+      template "app/core/chain_method.rb", "app/core/chain_method.rb"
+    end
+
+    def create_log_method
+      template "app/core/log_method.rb", "app/core/log_method.rb"
+    end
+
+    def create_messages
+      template "app/core/messages.rb", "app/core/messages.rb"
+    end
+    
     def create_logical_delete
-      template "logical_delete.rb", "app/models/concerns/"
+      template "app/models/concerns/logical_delete.rb", "app/models/concerns/logical_delete.rb"
     end
     
   end
