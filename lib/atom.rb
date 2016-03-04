81aafe08513fa09f19d5048493486b99aa1a335a
d318503 <satou-y93@mail.dnp.co.jp>
2016-03-04 18:28:17 +0900

    add logical delete module.

--- lib/atom.rb
+++ lib/atom.rb
@@ -3,9 +3,10 @@ require "atom/version"
 module Atom
 
   class InstallGenerator < ::Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-    def test
-      puts "test test"
+    def create_logical_delete
+      template "logical_delete.rb", "app/models/concerns/"
     end
     
   end
