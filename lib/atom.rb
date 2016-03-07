8a903cee4b01ec11a5282fbc479ba9f3c244d137
d318503 <satou-y93@mail.dnp.co.jp>
2016-03-07 11:36:29 +0900

    update setup scripts about rspec.

--- lib/atom.rb
+++ lib/atom.rb
@@ -1,4 +1,5 @@
 require "atom/version"
+require "rails/generators"
 
 module Atom
 
@@ -6,32 +7,46 @@ module Atom
     source_root File.expand_path('../templates', __FILE__)
 
     def add_gems
-      gem 'devise' #
+      gem 'devise'
       gem 'enumerize'
-      gem 'ridgepole' #
-      gem 'unicorn' #
-      gem 'hirb' #
-      gem 'hirb-unicode' #
+      gem 'ridgepole'
+      gem 'unicorn'
+      gem 'hirb'
+      gem 'hirb-unicode'
       gem_group :development, :test do
-        gem 'rspec-rails' #
-        gem 'factory_girl_rails' #
-        gem 'database_cleaner' #
-        gem 'pry-rails' #
+        gem 'rspec-rails'
+        gem 'factory_girl_rails'
+        gem 'database_cleaner'
+        gem 'pry-rails'
         gem 'pry-doc'
-        gem 'simplecov', :require => false #
+        gem 'simplecov', :require => false
       end
       gem_group :development do
-        gem "capistrano" #
-        gem "capistrano-rbenv", :git => 'https://github.com/capistrano/rbenv.git' #
-        gem "capistrano-rails" #
-        gem "capistrano-postgresql" #
-        gem "capistrano-bundler" #
+        gem "capistrano"
+        gem "capistrano-rbenv", :git => 'https://github.com/capistrano/rbenv.git'
+        gem "capistrano-rails"
+        gem "capistrano-postgresql"
+        gem "capistrano-bundler"
         gem "capistrano3-unicorn" 
+      end
+      
+      Bundler.with_clean_env do
+        run 'bundle install'
       end
     end
     
     def create_app_core_dir
       dir = "#{Rails.root}/app/core"
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    end
+    
+    def create_app_logics_dir
+      dir = "#{Rails.root}/app/logics"
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    end
+    
+    def create_spec_logics_dir
+      dir = "#{Rails.root}/spec/logics"
       FileUtils.mkdir_p(dir) unless File.directory?(dir)
     end
 
@@ -57,6 +72,31 @@ module Atom
     
     def create_logical_delete
       template "app/models/concerns/logical_delete.rb", "app/models/concerns/logical_delete.rb"
+    end
+
+    def create_schema_file_and_dir
+      template "db/Schemafile", "db/Schemafile"
+      dir = "#{Rails.root}/db/schema"
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    end
+
+    def rspec_init
+      run 'bin/rails generate rspec:install'
+    end
+
+    def overwrite_spec_helper
+      template "spec/spec_helper.rb", "spec/spec_helper.rb"
+    end
+
+    def gitignore_coverage
+      append_to_file '.gitignore', '/coverage'
+    end
+
+    def create_seed_dir
+      ["development", "test", "staging", "production"].each do |env|
+        dir = "#{Rails.root}/db/seeds/#{env}"
+        FileUtils.mkdir_p(dir) unless File.directory?(dir)
+      end
     end
     
   end
---
+++ lib/templates/db/seeds.rb
@@ -0,0 +1,2 @@
+
+require Rails.root.join("db/seeds/#{Rails.env}.rb")
